90508f639d0db17094d5d1f86e8ab8d286edc629
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-02-02 19:23:11 +0200

    Delete image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import Image from '../components/image';
 import SEO from '../components/seo';
 import Layout from '../layouts/layout';
 
-const HomePage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/about">Go to About page</Link>
-  </Layout>
-);
+const HomePage = ({ data }) => {
+  const { edges } = data.allMarkdownRemark;
+
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <h1>Hi, this is the home page</h1>
+      {edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>{node.frontmatter.title} </h3>
+          <p>{node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }} />
+      <Link to="/about">Go to About page</Link>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 100)
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default HomePage;
