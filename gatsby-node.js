0da730556b5dc990eecb8f7ad86449b4d8e65f16
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-02-07 20:24:54 +0300

    Implement onCreatePage API

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,9 @@
 const path = require('path');
 
+/**
+ * Implements the Gatsby API "createPages". This is called once the
+ * data layer is bootstrapped to let plugins create pages from data.
+ */
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
 
@@ -59,3 +63,32 @@ exports.createPages = ({ graphql, actions }) => {
     );
   });
 };
+
+/**
+ * Implements the Gatsby API "onCreatePage".
+ * This is called after page is created
+ */
+exports.onCreatePage = ({ page, actions }) => {
+  const { createPage, deletePage } = actions;
+  return new Promise(resolve => {
+    const oldPage = Object.assign({}, page);
+
+    /**
+     * Remove trailing slash unless page is "/"
+     */
+    page.path = replacePath(page.path);
+    if (page.path !== oldPage.path) {
+      /**
+       * Replace new page with old page
+       */
+      deletePage(oldPage);
+      createPage(page);
+    }
+    resolve();
+  });
+};
+
+/**
+ * Replacing '/' would result in empty string which is invalid
+ */
+const replacePath = path => (path === `/` ? path : path.replace(/\/$/, ``));
