4a16398a7f6d4392ea9ee9a666a75abdfc5a14b1
Lyubo Lyubchev <lyubo_2317@abv.bg>
2019-02-08 22:16:32 +0200

    Add tag page & template

---
+++ src/pages/tags.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TagsPage = () => {
+  return <div>Tags Page Here</div>;
+};
+
+export default TagsPage;
---
+++ src/templates/tags.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Tag = () => {
+  return <div>Tags Page Here</div>;
+};
+
+export default Tag;
